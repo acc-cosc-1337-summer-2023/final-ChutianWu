1a28400e25d43bc3b1612a760065ad8afcf36e55
ChutianWu <135079977+ChutianWu@users.noreply.github.com>
2023-07-30 04:58:38 +0000

    Question 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,53 @@
+#include<iostream>
+#include "die.h"
+#include "roll.h"
+#include "shooter.h"
+#include "phase.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
+
+using std::unique_ptr; 
+using std::make_unique;
+using std::vector;
+using std::ostream;
+using std::cout;
+
 int main() 
 {
-	
+srand(time(0));  
+
+    Die die1, die2;
+    Shooter shooter;
+
+    Roll* roll = shooter.throw_dice(die1, die2);  
+
+    ComeOutPhase come_out_phase;
+
+    while (come_out_phase.get_outcome(roll) == RollOutcome::natural || come_out_phase.get_outcome(roll) == RollOutcome::craps) {
+        // Display the Roll
+        cout<<"Rolled "<<roll->roll_value()<< ", roll again.\n";
+        // Roll again.
+        roll = shooter.throw_dice(die1, die2); 
+    }
+
+    cout<<"Rolled "<<roll->roll_value()<<", start of point phase.\n";
+    cout<<"Roll until "<<roll->roll_value()<<" or 7 is rolled.\n";
+    // Point is the roll value.
+    int point = roll->roll_value();  
+     // Roll again.
+    roll = shooter.throw_dice(die1, die2);
+
+    PointPhase point_phase(point);
+
+    while (point_phase.get_outcome(roll) != RollOutcome::point && point_phase.get_outcome(roll) != RollOutcome::seven_out) {
+        cout<<"Rolled "<< roll->roll_value()<<", roll again.\n";
+
+        roll = shooter.throw_dice(die1, die2);  
+    }
+
+    cout<<"Rolled "<<roll->roll_value()<<", end of point phase.\n";
+
+    cout<<shooter;
+
 	return 0;
 }
--- src/shooter.cpp
+++ src/shooter.cpp
@@ -5,7 +5,8 @@ unique_ptr<Roll> Shooter::throw_die(Die& die1, Die& die2)
 {
     unique_ptr<Roll> roll = make_unique<Roll>(die1, die2);
     roll->roll_die();
-    rolls.push_back(std::move(roll));
+    rolls.push_back(make_unique<Roll>(die1, die2));
+    rolls.back()->roll_die();
     return roll;
 }
 
